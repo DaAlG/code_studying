180a532ee7512d8f74588a101accbfb4eff2bb65
JEONHyeonHui <gmladk08@gmail.com>
2020-12-19 21:58:46 +0900

    201219

---
+++ BFS(연습)/16928_jhh.cpp
@@ -0,0 +1,62 @@
+﻿/*
+100개의 칸으로 나누어져 있는 게임. 1에서 100으로 가야 한다.
+
+주사위를 굴려 나온 수 만큼 이동할 수 있으며,
+도착한 칸이 사다리인 경우에는 사다리를 타고 더 큰 칸으로
+뱀인 경우에는 더 작은 번호의 칸으로 이동한다.
+
+사다리와 뱀 둘 다인 경우는 없다.
+
+주사위에 나온 수를 정할 수 있을 때, 최소 몇번 굴려야하는지 구하는 문제
+
+시작점-1 도착점-100
+어떤 칸 x 에서 1,2,3,4...가 나오는 경우
+
+*/
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#define next _next
+//next[x]=y : x->y로 이동했을 때 그 다음 칸의 정보를 저장
+//뱀이나 사다리인 경우 next[x]=y
+//일반 칸인 경우 next[x]=x
+using namespace std;
+int dist[101];
+int next[101];//일반 칸
+int main() {
+	int n, m;
+	cin >> n >> m;
+	for (int i = 1; i <= 100; i++) {
+		next[i] = i;//사다리나 뱀을 사용한 경우
+		dist[i] = -1;//거리 -1로 초기화
+	}
+	while (n--) {
+		int x, y;
+		cin >> x >> y;
+		next[x] = y;//사다리
+	}
+	while (m--) {
+		int x, y;
+		cin >> x >> y;
+		next[x] = y;
+	}//뱀
+	dist[1] = 0;
+	queue<int> q;
+	q.push(1);//시작점에 대한 처리
+	while (!q.empty()) {
+		int x = q.front(); q.pop();
+		for (int i = 1; i <= 6; i++) {
+			int y = x + i;
+			if (y > 100) continue;
+			y = next[y];
+
+			//BFS
+			if (dist[y] == -1) {
+				dist[y] = dist[x] + 1;
+				q.push(y);
+			}
+		}
+	}
+	cout << dist[100] << '\n';//100번 칸 까지 가는데 필요한 칸의 최소
+	return 0;
+}
---
+++ BFS(연습)/16948_jhh.cpp
@@ -0,0 +1,39 @@
+﻿/*
+크기가 n*n인 체스판과 두 칸이 주어졌을 때 최소 이동 횟수
+5<=n<=200
+0(n^2)
+미로 탈출의 조금 변형된 버전
+*/
+#include<iostream>
+#include<tuple>
+#include<cstring>
+#include<queue>
+using namespace std;
+int dx[] = { -2,-2,0,0,2,2 }; //이동할 수 있는 방법
+int dy[] = { -1,1,-2,2,-1,1 }; 
+int dist[200][200]; //200*200
+int main() {
+	int n;
+	cin >> n;
+	int sx, sy, ex, ey;
+	cin >> sx >> sy >> ex >> ey;
+	memset(dist, -1, sizeof(dist));
+	dist[sx][sy] = 0;//시작점
+	queue<pair<int, int>> q;
+	q.push(make_pair(sx, sy));//queue에 넣는 것
+	while (!q.empty()) {
+		int x, y;
+		tie(x, y) = q.front(); q.pop();
+		for (int k = 0; k < 6; k++) {
+			int nx = x + dx[k];
+			int ny = y + dy[k];
+			if(0<=nx&&nx<n&&0<=ny&&ny<n)//범위 검사
+				if (dist[nx][ny] == -1) {
+					q.push(make_pair(nx, ny));
+					dist[nx][ny] = dist[x][y] + 1;
+				}
+		}
+	}
+	cout << dist[ex][ey] << '\n';
+	return 0;
+}
