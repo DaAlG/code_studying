95079a5276ec23c616495c3d08fc3a6b78643cdb
JEONHyeonHui <gmladk08@gmail.com>
2020-12-27 17:52:40 +0900

    201227

---
+++ BFS(연습)/12886_돌 그룹_jhh.cpp
@@ -0,0 +1,56 @@
+﻿/*
+돌이 3개의 그룹으로 나누어져 있고, 각 그룹에는 돌이 A,B,C개 있다.
+돌은 단계별로 움직이고, 각 단계는 다음과 같다
+크기가 같지 않은 두 그룹을 고른다. 돌의 개수가 작은 쪽을 X, 큰 쪽을 Y 라고 한다.
+X에 있는 돌의 개수를 X+X개로,Y에 있는 돌의 개수를 Y-X로 만든다.
+A,B,C가 주어졌을 때, 모든 그룹에 있는 돌의 개수를 같게 만들 수 있는 지 구하는 문제
+
+Y에서 X를 넘겨준다고 보면 됨
+DFS, BFS 모두 다 풀 수 있음 : 하나의 시작점이 있을 때 이동할 수 있는지 없는지 볼 수 있는 것과 같음
+
+(A,B,C) : 세 개의 정보 => 정점의 정보
+돌의 개수가 매우 커질 수가 있음
+1500^3 => 33억 =3기가
+공간도 너무 많이 필요하고 시간도 많이 걸림
+
+정점: (A,B,C)로하면 공간이 너무 많이 필요하기 떄문에 (A,B)로 설정하고 해결해도 됨
+전체 정점의 개수: A+B+C
+A와 B를 알고 있다면 C를 구할 수 있음 => 1500^2
+*/
+#include <iostream>
+#include <queue>
+using namespace std;
+bool check[1501][1501];
+int sum;
+void go(int x, int y) { //가능한 구성인지 확인 DFS임
+    if (check[x][y]) return;
+    check[x][y] = true;
+    int a[3] = { x, y, sum - x - y }; // 돌 저장
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (a[i] < a[j]) {
+                int b[3] = { x, y, sum - x - y };
+                b[i] += a[i];
+                b[j] -= a[i];
+                go(b[0], b[1]);//가능한지 check
+            }
+        }
+    }
+}
+int main() {
+    int x, y, z;
+    cin >> x >> y >> z;
+    sum = x + y + z;
+    if (sum % 3 != 0) {
+        cout << 0 << '\n';
+        return 0;// 3으로 나누어 떨어지지 않으면 돌의 개수 똑같아 질 수 없음
+    }
+    go(x, y); //x,y이용하여 go
+    if (check[sum / 3][sum / 3]) {
+        cout << 1 << '\n';
+    }
+    else {
+        cout << 0 << '\n';
+    }
+    return 0;
+}
---
+++ BFS(연습)/14502_연구소_jhh.cpp
@@ -0,0 +1,102 @@
+﻿/*
+N*M의 직사각형 지도가 있고, 1*1크기의 칸으로 나누어져 있다.
+칸: 빈 칸과 벽으로 이루어져 있음
+일부 빈 칸에는 바이러스가 있고, 인접한 빈 칸으로 계속해서 퍼져 나간다.
+벽을 3개 세워서 바이러스가 퍼질 수 없는 곳의 크기를 구하는 문제
+
+1. 벽을 3개 세움
+2. 바이러스가 퍼질 수 없는 곳의 크기 =>BFS, DFS 가능
+하나의 정점에서 시작해서 연결된 모든 정점을 방문함
+
+<BFS 와 DFS 의 목적>
+한 정점에서 시작해 연결된 모든 정점을 방문하는 것
+
+시간 복잡도 O(NM)
+
+벽을 3개 세우는 경우의 수: (NM)^3
+총 (NM)^4시간이 걸림
+*/
+#include <iostream>
+#include <queue>
+using namespace std;
+int n, m;
+int a[10][10];
+int b[10][10];
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+int bfs() {
+    queue<pair<int,int>> q;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            b[i][j] = a[i][j];
+            if (b[i][j] == 2) { //바이러스의 위치
+                q.push(make_pair(i,j));
+            }
+        }
+    }
+    while (!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for (int k=0; k<4; k++) {
+            int nx = x+dx[k];
+            int ny = y+dy[k];
+            if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+                if (b[nx][ny] == 0) {
+                    b[nx][ny] = 2;//바이러스 퍼질 수 있는 칸
+                    q.push(make_pair(nx,ny));
+                }
+            }
+        }
+    }
+    int cnt = 0;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            if (b[i][j] == 0) {//바이러스 퍼지지 않은 칸
+                cnt += 1;
+            }
+        }
+    }
+    return cnt;
+}
+int main() {
+    cin >> n >> m;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            cin >> a[i][j];
+        }
+    }
+    int ans = 0;
+    for (int x1=0; x1<n; x1++) {
+        for (int y1=0; y1<m; y1++) {
+            if (a[x1][y1] != 0) continue;
+            for (int x2=0; x2<n; x2++) {
+                for (int y2=0; y2<m; y2++) {
+                    if (a[x2][y2] != 0) continue;
+                    for (int x3=0; x3<n; x3++) {
+                        for (int y3=0; y3<m; y3++) {
+                            if (a[x3][y3] != 0) continue;
+                            if (x1 == x2 && y1 == y2) continue;
+                            if (x1 == x3 && y1 == y3) continue;
+                            if (x2 == x3 && y2 == y3) continue;//3개의 쌍이 같을 수 있으니까 비교
+                            a[x1][y1] = 1;
+                            a[x2][y2] = 1;
+                            a[x3][y3] = 1;
+                            int cur = bfs();//BFS돌고 바꿔주기
+                            if (ans < cur) ans = cur;
+                            a[x1][y1] = 0;
+                            a[x2][y2] = 0;
+                            a[x3][y3] = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    cout << ans << '\n';
+    return 0;
+}
+/*
+재귀함수를 구현하려면 머리가 복잡해서 for 문으로 많이 구현
+코드에다가 3개를 쓰는 경우가 많음
+*/
