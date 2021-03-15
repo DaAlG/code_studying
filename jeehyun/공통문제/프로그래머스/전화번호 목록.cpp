c867660f05d97a34ce0c29ba3653fabb7ddbeb6d
W_Jeehyun <wjh51333@gmail.com>
2021-03-15 23:58:37 +0900

    210315 전화번호 목록 수정

--- jeehyun/공통문제/프로그래머스/전화번호 목록.cpp
+++ jeehyun/공통문제/프로그래머스/전화번호 목록.cpp
@@ -28,8 +28,21 @@ bool solution(vector<string> phone_book)
 {
     bool answer = true;
 
+    unordered_map<string, int> hash_map;
+    for (int i = 0; i < phone_book.size(); i++) {
+        hash_map[phone_book[i]] = 1;
+    }
 
+    for (int i = 0; i < phone_book.size(); i++) {
+        string phone_number = "";
+        for (int j = 0; j < phone_book[i].size(); j++) {
+            phone_number += phone_book[i][j];
+            if (hash_map[phone_number] && phone_number != phone_book[i])
+                answer = false;
+        }
+    }
 
+    return answer;
 }
 */
 
