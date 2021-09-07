9f8a76f49bbee0f2809c3915a9cfa01fc529bf78
szzi <kamuuim@naver.com>
2021-09-07 23:55:20 +0900

    0907

--- seojin/개별문제/0906/src/Solution_가장큰수.java
+++ seojin/개별문제/0906/src/Solution_가장큰수.java
@@ -1,28 +1,33 @@
-import java.util.*;
-
-public class Solution_가장큰수 {
-
-    public static String solution(int[] numbers) {
+// https://developerdk.tistory.com/24
+// 쉬운 줄 알았는데 잘 안풀렸음
+import java.util.Arrays;
+import java.util.Comparator;
+ 
+class Solution {
+    public String solution(int[] numbers) {
         String answer = "";
         
-        for(int n : numbers) {
-        	answer += n;
+        //int 배열을 String 배열로 변환
+        String[] arr = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            arr[i] = (String.valueOf(numbers[i])); // String.valueOf()가 ""+int보다 효율적!!
         }
         
-        char[] ans = answer.toCharArray();
-        Arrays.sort(ans);
+        //배열 정렬, 정렬 규칙으로는 2개를 더하여 더 큰 쪽이 우선순위가 있도록 정렬
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return (s2+s1).compareTo(s1+s2);
+            }
+        });
         
-        StringBuilder sb = new StringBuilder();
-        for (int i = ans.length-1; i>=0; i--) {
-			sb.append(ans[i]);
-		}
+        //0000 처럼 0으로만 구성되어있으면 0 return
+        if (arr[0].equals("0")) return "0";
         
-     
-        return sb.toString();
+        //그 외의 경우 순차적으로 연결하여 answer return
+        for (int i = 0; i < arr.length; i++) {
+            answer+=arr[i];
+        }
+        return answer;
     }
-    
-    public static void main(String[] args) {
-		solution(new int[]{6,10,2});
-	}
-
 }
---
+++ seojin/개별문제/0907/src/Solution_다리를지나는트럭.java
@@ -0,0 +1,9 @@
+
+import java.util.*;
+
+class Solution_다리를지나는트럭 {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        return answer;
+    }
+}
